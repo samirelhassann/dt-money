9e78e4429bffe1265e85de130e0f801e5df0d027
Samir El Hassan <samirelhassann@gmail.com>
2024-02-03 11:39:02 -0300

    ci: create the deploy.yml to deploy the application on github pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 import { ThemeProvider } from "styled-components";
 
-import { TransactionsProvider } from './contexts/TransactionsContext';
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 import { Transactions } from "./pages/Transactions";
 
 function App() {
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,12 +9,13 @@ export const GlobalStyle = createGlobalStyle`
 
     :focus{
         outline: 0;
-        box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
+        box-shadow: 0 0 0 2px ${(props: any) => props.theme["green-500"]};
     }
 
     body{
-        background-color: ${(props) => props.theme["gray-800"]};
-        color: ${(props) => props.theme["gray-100"]};
+        // @ts-nocheck
+        background-color: ${(props: any) => props.theme["gray-800"]};
+        color: ${(props: any) => props.theme["gray-100"]};
         -webkit-font-smoothing: antialiased;
     }
 
